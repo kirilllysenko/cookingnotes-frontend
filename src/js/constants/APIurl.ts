512d7ab4520f704a78674995f6444d90bca79a16
Kirill Lysenko <kirill.lysenko.msk@gmail.com>
2022-04-05 23:56:21 +0300

    fixed bugs

--- src/js/constants/APIurl.ts
+++ src/js/constants/APIurl.ts
@@ -1,6 +1,6 @@
-export const API_BASE_URL = 'https://backend.cookingnotes.cyprog.net/api';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export const ACCESS_TOKEN = 'accessToken';
 
-export const OAUTH2_REDIRECT_URI = `https://cookingnotes.cyprog.net/oauth2/redirect`;
+export const OAUTH2_REDIRECT_URI = process.env.REACT_APP_OAUTH2_REDIRECT_URI;
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
